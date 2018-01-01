206403b30ab2c7c1a700dedd3ab7c4a6de1816b6
janko <molo226@vp.pl>
2018-01-01 19:35:17 +0100

    Added modifications from major DIX directoriesy

--- DIX/DIX/settings.py
+++ DIX/DIX/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+  #  'django.meals',
+  #  'django.exercises',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
